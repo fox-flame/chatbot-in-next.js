ed3d400f9ca870c80ab39e5dde3891bd31f6bc84
Hamza Shah <hamza.habib@clientpoint.net>
2024-05-01 23:47:07 +0500

    feat: ChatBot AI

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
+import { ChatHistory } from "@/utils/shared";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -20,35 +21,53 @@ export default async function Index() {
   const isSupabaseConnected = canInitSupabaseClient();
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
-          {isSupabaseConnected && <AuthButton />}
-        </div>
+    <div className="flex-1 w-full flex flex-col gap-5 items-center max-w-4xl p-[10px]">
+      <nav className="animate-in w-full flex items-center border-b border-b-foreground/10 h-16">
+        <p>An AI-Powered Chat Bot - Ask me anything</p>
       </nav>
-
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
+      <div className="w-full max-h-[500px] flex-1 overflow-y-scroll">
+        {ChatHistory.map((value, index) => {
+          return (
+            <div
+              key={index}
+              className={`flex gap-2 mb-6 ${
+                value.role === "assistant" && "flex-row-reverse"
+              }`}
+            >
+              <div
+                className={`w-9 h-9 rounded-full flex items-center justify-center ${
+                  value.role === "user" ? "bg-orange-900" : "bg-indigo-900"
+                }`}
+              >
+                {value.role === "user" ? "A" : "B"}
+              </div>
+              <div
+                className={`w-10/12 rounded-xl ${
+                  value.role === "user" ? "bg-zinc-900" : "bg-sky-950	"
+                } p-3`}
+              >
+                {value.content}
+              </div>
+              <div></div>
+            </div>
+          );
+        })}
       </div>
 
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
+      <div className="animate-in w-full h-20 flex items-center pr-3 border rounded-xl relative">
+        <textarea
+          className="h-10 resize-none placeholder:text-gray-400 bg-transparent flex-1 py-2 pl-9 pr-3 focus:outline-none sm:text-sm"
+          placeholder="Ask anything..."
+        />
+        <button
+          className="h-16 hover:bg-foreground/10 w-24 border rounded-xl"
+          disabled={false}
+        >
+          Send
+        </button>
+      </div>
+
+      {/* <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3"></div> */}
     </div>
   );
 }
---
+++ utils/shared/index.ts
@@ -0,0 +1,68 @@
+export const ChatHistory = [
+  {
+    role: "assistant",
+    content:
+      "As an owner of this project, you can view and manage all API keys in this project.",
+  },
+  {
+    role: "user",
+    content:
+      "Ok sure, you means i can view and manage all API keys in this project? Right.",
+  },
+  {
+    role: "assistant",
+    content:
+      "Yes, you can view and manage all API keys in this project which you can edit or delete or create new.",
+  },
+  {
+    role: "user",
+    content:
+      "Now tell me the bandwidth of this supabase, i mean the DB size and request/sec or minute.",
+  },
+  {
+    role: "assistant",
+    content:
+      "As of my last knowledge, i'm not sure but you can find that out in their website.",
+  },
+  {
+    role: "user",
+    content: "Thank you, Appreciate it",
+  },
+  {
+    role: "assistant",
+    content: "Happy to help!",
+  },
+  {
+    role: "assistant",
+    content:
+      "As an owner of this project, you can view and manage all API keys in this project.",
+  },
+  {
+    role: "user",
+    content:
+      "Ok sure, you means i can view and manage all API keys in this project? Right.",
+  },
+  {
+    role: "assistant",
+    content:
+      "Yes, you can view and manage all API keys in this project which you can edit or delete or create new.",
+  },
+  {
+    role: "user",
+    content:
+      "Now tell me the bandwidth of this supabase, i mean the DB size and request/sec or minute.",
+  },
+  {
+    role: "assistant",
+    content:
+      "As of my last knowledge, i'm not sure but you can find that out in their website.",
+  },
+  {
+    role: "user",
+    content: "Thank you, Appreciate it",
+  },
+  {
+    role: "assistant",
+    content: "Happy to help!",
+  },
+];
